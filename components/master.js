5797cc99b22056902b6da52d0504503b3e15adc9
Dhiraj KC <62998336+DKC195@users.noreply.github.com>
2022-02-06 16:11:04 +0545

    Testing Feb 06

---
+++ components/master.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+  $('nav').load("./components/main/nav/nav.html");
+});
+
+$(document).ready(function (){
+  $('footer').load("./components/main/footer/footer.html");
+});
